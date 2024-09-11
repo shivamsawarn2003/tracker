aeda436e416478a28618c69fff49d96df1f3247f
shivamkvs <shivamsawarn2003@gmail.com>
2024-09-11 19:12:05 +0530

    final commit

--- react/src/pages/Login.js
+++ react/src/pages/Login.js
@@ -27,7 +27,7 @@ function Login() {
             return handleError('email and password are required')
         }
         try {
-            const url = `http://localhost:5000/auth/login`;
+            const url = `https://tracker-3-5rem.onrender.com/auth/login`;
             const response = await fetch(url, {
                 method: "POST",
                 headers: {
--- react/src/pages/Signup.js
+++ react/src/pages/Signup.js
@@ -27,7 +27,7 @@ function Signup() {
             return handleError('name, email and password are required')
         }
         try {
-            const url = `http://localhost:5000/auth/signup`;
+            const url = `https://tracker-3-5rem.onrender.com/auth/signup`;
             const response = await fetch(url, {
                 method: "POST",
                 headers: {
