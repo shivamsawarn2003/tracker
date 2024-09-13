db653d22f2f7c3e6c04dafc3593a95e7a39b5a5e
shivamkvs <shivamsawarn2003@gmail.com>
2024-09-14 00:39:02 +0530

    final commit

--- node_modules/frontend/src/pages/Home.js
+++ node_modules/frontend/src/pages/Home.js
@@ -74,7 +74,7 @@ function Home() {
 
   const fetchProducts = async () => {
     try {
-      const url = "https://tracker-5.onrender.com/home";
+      const url = "https://tracker-5.onrender.com/products";
       const headers = {
         headers: {
           'Authorization': localStorage.getItem('token'),
