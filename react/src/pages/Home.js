a30e31eba4a3279bb9b18c61543896cb0752cb72
shivamkvs <shivamsawarn2003@gmail.com>
2024-09-14 09:59:50 +0530

    final commit

--- react/src/pages/Home.js
+++ react/src/pages/Home.js
@@ -3,12 +3,11 @@ import { useNavigate } from 'react-router-dom';
 import axios from 'axios';
 import { ToastContainer } from 'react-toastify';
 import AuthContext  from '../AuthContext'; // Ensure this path is correct
-import { handleError, handleSuccess } from '../utils';
+import {  handleSuccess } from '../utils';
 import '../App.css'; // Assuming you need styles from App.css
 
 function Home() {
   const [loggedInUser, setLoggedInUser] = useState('');
-  const [products, setProducts] = useState([]);
   const [transactionDate, setTransactionDate] = useState("");
   const [description, setDescription] = useState("");
   const [amount, setAmount] = useState("");
@@ -72,26 +71,6 @@ function Home() {
     }, 1000);
   };
 
-  const fetchProducts = async () => {
-    try {
-      const url = "https://tracker-5.onrender.com/products";
-      const headers = {
-        headers: {
-          'Authorization': localStorage.getItem('token'),
-        },
-      };
-      const response = await fetch(url, headers);
-      const result = await response.json();
-      setProducts(result);
-    } catch (err) {
-      handleError(err);
-    }
-  };
-
-  useEffect(() => {
-    fetchProducts();
-  }, []);
-
   return (
     <div className="App">
       <h1>Welcome {loggedInUser}</h1>
@@ -137,14 +116,6 @@ function Home() {
               <div className="price">${transaction.amount}</div>
             </div>
           </div>
-        ))}
-      </div>
-
-      <div>
-        {products && products.map((item, index) => (
-          <ul key={index}>
-            <span>{item.name} : {item.price}</span>
-          </ul>
         ))}
       </div>
 
